eeb7f30d2012a8bb2ddc9c330cd3edb93aec4125
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-05-22 18:56:25 -0300

    added pagination

--- src/components/HomePage/ProductsList.tsx
+++ src/components/HomePage/ProductsList.tsx
@@ -1,27 +1,11 @@
-import { useEffect } from "react";
-import { api } from "../../lib/api";
 import { ProductsTableItem } from "./ProductsTableItem";
-import { selectProducts, setProducts } from "../../redux/productsSlice";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import Product from "../../models/Product";
 
-export function ProductList() {
-  const products: Product[] = useAppSelector(selectProducts) || [];
-  const dispatch = useAppDispatch();
+interface ProductListProps {
+  products: Product[]
+}
 
-  useEffect(() => {
-    async function getProducts() {
-      try {
-        const { data: products } = await api.get("/products");
-        dispatch(setProducts(products));
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    getProducts();
-  }, []);
-
+export function ProductList({ products }: ProductListProps) {
   return (
     <>
       {products == []
--- src/components/HomePage/ProductsTable.tsx
+++ src/components/HomePage/ProductsTable.tsx
@@ -1,21 +1,89 @@
+import { useEffect, useState } from "react";
+import { api } from "../../lib/api";
+import Product from "../../models/Product";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { selectProducts, setProducts } from "../../redux/productsSlice";
 import { ProductList } from "./ProductsList";
 
+const resultsPerPage = 10;
+
 export function ProductsTable() {
+  const dispatch = useAppDispatch();
+
+  const [page, setPage] = useState(1);
+
+  const allProducts: Product[] = useAppSelector(selectProducts) || [];
+  const products =
+    allProducts.length <= resultsPerPage
+      ? allProducts
+      : allProducts.slice((page - 1) * 10, page * 10);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const { data: products } = await api.get("/products");
+        dispatch(setProducts(products));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getProducts();
+  }, []);
+
+  function getPaginationGroup() {
+    const pageCount = Math.ceil(allProducts.length / resultsPerPage);
+
+    return [...new Array(pageCount)].map((_, index) => index + 1);
+  }
+
+  function handleChangePage(page: number) {
+    setPage(page);
+  }
+
   return (
-    <table className="w-full h-min table-fixed border-collapse text-left">
-      <thead className="pl-">
-        <tr className="bg-slate-700 h-8 ">
-          <th>Produto</th>
-          <th>Data de fabricação</th>
-          <th>Data de validade</th>
-          <th>Preço</th>
-          <th className="w-8"></th>
-          <th className="w-8"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <ProductList />
-      </tbody>
-    </table>
+    <div>
+      <table className="w-full h-min table-fixed border-collapse text-left">
+        <thead className="pl-">
+          <tr className="bg-slate-700 h-8 ">
+            <th>Produto</th>
+            <th>Data de fabricação</th>
+            <th>Data de validade</th>
+            <th>Preço</th>
+            <th className="w-8"></th>
+            <th className="w-8"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <ProductList products={products} />
+        </tbody>
+      </table>
+      {allProducts.length <= resultsPerPage ? (
+        ""
+      ) : (
+        <div className="bg-green-500 flex justify-center gap-2">
+          {page > 1 ? (
+            <button onClick={() => handleChangePage(page - 1)}>{"<"}</button>
+          ) : (
+            ""
+          )}
+
+          {getPaginationGroup().map((currPage) => (
+            <button
+              onClick={() => handleChangePage(currPage)}
+              disabled={currPage === page}
+            >
+              {currPage}
+            </button>
+          ))}
+
+          {page < Math.ceil(allProducts.length / resultsPerPage) ? (
+            <button onClick={() => handleChangePage(page + 1)}>{">"}</button>
+          ) : (
+            ""
+          )}
+        </div>
+      )}
+    </div>
   );
 }
