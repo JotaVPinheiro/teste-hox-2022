db9380ae77664f0a9da64aa26c5b6363b0f0deca
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-05-22 23:04:25 -0300

    updated login page

--- src/components/LoginPage/LoginForm.tsx
+++ src/components/LoginPage/LoginForm.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
-import { verifyLogin } from "../../auth";
-import { logIn } from "../../redux/userSlice";
 import { useNavigate } from "react-router-dom";
+import { CircleNotch } from "phosphor-react";
+
 import { useAppDispatch } from "../../redux/hooks";
+import { logIn } from "../../redux/userSlice";
+import { verifyLogin } from "../../auth";
 
 function initialState() {
   return { email: "", password: "" };
 }
 
 export function LoginForm() {
-  const [values, setValues] = useState(initialState);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  function handleLogin(event: React.FormEvent<HTMLFormElement>) {
+  const [values, setValues] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleLogin() {
+    setIsLoading(true);
     const token = verifyLogin(values);
 
     if (!token) {
@@ -21,45 +26,53 @@ export function LoginForm() {
       return;
     }
 
+    setIsLoading(false);
     dispatch(logIn(token));
     navigate("/");
   }
 
   function onChange(event: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target;
-
     setValues({ ...values, [name]: value });
   }
 
   return (
     <form onSubmit={handleLogin}>
-      <fieldset className="flex flex-col justify-center gap-3 p-10">
+      <fieldset className="flex flex-col gap-2 p-10">
         <span className="flex justify-center text-3xl pb-6">Login</span>
+        <label className="text-sm " htmlFor="email">
+          E-mail:
+        </label>
         <input
           id="email"
           name="email"
           type="text"
-          placeholder="E-mail"
           onChange={onChange}
           value={values.email}
-          className="w-full rounded-full border-2 border-emerald-500 text-sm 
-            text-white p-3 bg-transparent placeholder:text-sm outline-none"
+          placeholder="admin@hox.rs"
+          className="w-full rounded-lg ring-[1px] ring-zinc-600 placeholder:text-zinc-500 text-sm text-white p-3 bg-transparent hover:ring-zinc-500 focus:ring-indigo-500 focus:ring-2 outline-none"
         />
+        <label className="mt-2 text-sm" htmlFor="password">
+          Senha:
+        </label>
         <input
           id="password"
           name="password"
           type="password"
-          placeholder="Senha"
           onChange={onChange}
           value={values.password}
-          className="w-full rounded-full border-2 border-emerald-500 text-sm 
-            text-white p-3 bg-transparent placeholder:text-sm outline-none"
+          placeholder="admin"
+          className="w-full rounded-lg ring-[1px] ring-zinc-600 placeholder:text-zinc-500 text-sm text-white p-3 bg-transparent hover:ring-zinc-500 focus:ring-indigo-500 focus:ring-2 outline-none"
         />
         <button
           type="submit"
-          className="w-36 h-12 rounded-full border-2 border-teal-500 text-sm"
+          className="flex justify-center items-center mt-2 w h-12 rounded-lg bg-indigo-600 text-md shadow-md outline-none hover:bg-opacity-50 focus:bg-opacity-50 transition-colors"
         >
-          Entrar
+          {isLoading ? (
+            <CircleNotch weight="bold" className="w-4 h-4 animate-spin" />
+          ) : (
+            "Entrar"
+          )}
         </button>
       </fieldset>
     </form>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "../components/LoginPage/LoginForm";
 export function LoginPage() {
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
-      <div className="bg-zinc-900 rounded-md w-96">
+      <div className="bg-zinc-900 rounded-lg w-96 shadow-xl">
         <LoginForm />
       </div>
     </div>
