dbba66eee960236402cf1b13938e0121e281693e
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-05-22 21:10:34 -0300

    added jwt to axios requests

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,18 @@
 import axios from "axios";
+const { token } = JSON.parse(localStorage.getItem("persist:root") || "");
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+api.interceptors.request.use(async (config) => {
+  if (config.headers === undefined) {
+    config.headers = {};
+  }
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
